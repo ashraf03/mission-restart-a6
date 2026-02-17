988dbc07e52cc7a5a3e5dbf0788246d634753a07
ashraf03 <eng.ashrafuddin.lus@gmail.com>
2026-02-17 23:16:49 +0600

    Added some function with tailwind css  in scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,5 +18,79 @@ const productsLoad = (products) => {
     });
 }
 
+const levelProduct = (category) => {
+    // console.log(category);
+    fetch(`https://fakestoreapi.com/products/category/${category}`)
+    .then(res => res.json())
+    .then(data => levelProductShow(data))
+}
 
+const allProduct = async () => {
+
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+
+    allLevelProductShow(data);   
+};
+
+const allLevelProductShow = (products) => {
+
+    const containerProduct = document.getElementById("containerProduct");
+    containerProduct.innerHTML = "AShraf";
+
+    products.forEach(ele => {
+
+        const productDetails = document.createElement("div");
+
+        productDetails.innerHTML = `
+            <div class="space-y-5">
+                <img src="${ele.image}" alt="" class="w-[300px]">
+                
+                <div class="flex justify-between">
+                    <span>${ele.category}</span>
+                    <span>
+                        <i class="fa-regular fa-star"></i> ${ele.rating.rate}
+                    </span>
+                </div>
+
+                <h2>${ele.title}</h2>
+                <span>$${ele.price}</span>
+
+                <div class="flex justify-between">
+                    <button class="btn btn-soft">Default</button>
+                    <button class="btn btn-primary">Primary</button>
+                </div>
+            </div>
+        `;
+
+        containerProduct.append(productDetails);
+    });
+};
+const levelProductShow = (productShow) => {
+    const containerProduct = document.getElementById("containerProduct")
+    // containerProduct.innerHTML = "";
+    
+    productShow.forEach(ele => {
+        const productDetails = document.createElement("div");
+        console.log(productDetails)
+        productDetails.innerHTML = `
+                <div class="space-y-5">
+        <img src="${ele.image}" alt="" class="w-[300px]">
+        <div class="flex justify-between">
+          <span>${ele.category}</span>
+          <span><i class="fa-regular fa-star"></i> ${ele.rating.rate}</span>
+        </div>
+        <h2>Men's cotton Jens</h2>
+        <span>${ele.price}</span>
+        <div class="flex justify-between">
+          <button class="btn btn-soft">Default</button>
+          <button class="btn btn-primary">Primary</button>
+        </div>
+      </div>
+        `;
+    containerProduct.append(productDetails)
+    });
+}
+
+levelProduct()
 products();
